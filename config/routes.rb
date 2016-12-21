4924b0e5b6a9052ad0e9e7c3d598f37cbf292462
Sean Yeoh <sean.yeoh@hotmail.com>
2016-12-21 16:08:12 +0800

    Create users table Create users and sessions controller Added user authentications Added navbar Added bootstrap gem Added welcome controller and root path to welcome#index Added role to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:create]
+
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
